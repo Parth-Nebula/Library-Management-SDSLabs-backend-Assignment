7bc3a647239ca24096a6f73141898ee0d30835a9
Parth-Nebula <99603309+Parth-Nebula@users.noreply.github.com>
2022-06-02 16:48:55 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql=require('mysql2');
 module.exports=mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: ""
+    password: "",
     database: "mysql",
     port:3306,
   });
